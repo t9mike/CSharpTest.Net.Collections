04f5d2f4c626dee4008c875f519bbc5b97392aea
Muhammad Miftah <mamift@gmail.com>
2019-09-18 09:34:42 +1000

    Ported to .NET Standard 2.0. Some tests (8 out 641) fail.

--- src/CSharpTest.Net.Collections/Properties/AssemblyInfo.cs
+++ src/CSharpTest.Net.Collections/Properties/AssemblyInfo.cs
@@ -15,16 +15,5 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("CSharpTest.Net.Collections.dll")]
-[assembly: AssemblyDescription("Fully managed B+ Tree implementation for local data storage.")]
-[assembly: AssemblyProduct("http://CSharpTest.Net/Projects")]
-[assembly: AssemblyConfiguration("Debug")]
-
-[assembly: AssemblyCompany("Roger Knapp")]
-[assembly: AssemblyCopyright("Copyright 2009 by Roger Knapp, Licensed under the Apache License, Version 2.0")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: ObfuscateAssembly(false)]
 [assembly: ComVisibleAttribute(false)]
